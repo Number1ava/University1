a86735ca05a193b3c631ebeedefea538ead3edf0
Mirzoavaz Khamdamov <73744057+Number1ava@users.noreply.github.com>
2022-05-25 07:41:27 +0500

    add new uz/ru language, add style lang

--- app/js/language.js
+++ app/js/language.js
@@ -76,9 +76,92 @@ const langArr = {
     uz: 'Kafedra: qishki sport turlari nazariyasi va uslublari',
     ru: 'Кафедра: теории и методики зимних видов спорта',
   },
+  // Секция с количеством студентов,преподователей и т.д
+  quantity1: {
+    uz: "Talabalar soni",
+    ru: "Количество студентов",
+  },
+  quantity2: {
+    uz: "O'qituvchilar",
+    ru: "Преподаватели",
+  },
+  quantity3: {
+    uz: "Hamkorlik",
+    ru: "Сотрудничество",
+  },
+  quantity4: {
+    uz: "Kurs",
+    ru: "Курс",
+  },
+  // название секции видах спорта кафедры
+  sports_title: {
+    uz: "Kafedraning sport turlari",
+    ru: "Спортивные виды кафедры",
+  },
+  // виды спорта
+  sports_name1: {
+    uz: "Xokkey",
+    ru: "Хоккей",
+  },
+  sports_descr1: {
+    uz: "Muz ustidagi jamoaviy sport o'yini, konkida ikki jamoa o'rtasidagi qarama-qarshilik.",
+    ru: "Командная спортивная игра на льду, противостояние двух команд на коньках.",
+  },
+  sports_name2: {
+    uz: "Chim ustida xokkey",
+    ru: "Хоккей на траве",
+  },
+  sports_descr2: {
+    uz: "Jamoaviy sport turi (har bir jamoada 11 kishi) ikkita jamoa tomonidan tayoq va to'p yordamida o'ynaydi.",
+    ru: "В командном виде спорта (по 11 человек в каждой) участвуют две команды, использующие клюшки и мячи.",
+  },
+  sports_name3: {
+    uz: "Tog' chang'i",
+    ru: "Горнолыжный ",
+  },
+  sports_descr3: {
+    uz: "Sport, uning mohiyati tog'lar bo'ylab maxsus bayroqlar va darvozalar bilan belgilangan yo'l bo'ylab chang'i uchishdir.",
+    ru: "Спорт, суть которого заключается в катании на лыжах по дороге, обозначенной специальными флажками и воротами через горы.",
+  },
+  sports_name4: {
+    uz: "Figurali uchish",
+    ru: "Фигурное катание",
+  },
+  sports_descr4: {
+    uz: "Asosiy g'oya - sportchini yoki bir juft sportchini konkida muz ustida harakatlantirish va musiqaga maxsus elementlarni bajarish.",
+    ru: "Основная идея в передвижении спортсмена или пары спортсменов по льду на коньках и выполнении специальных элементов под музыку.",
+  },
+  // Секция с преподователями
+  teachers_title: {
+    uz: "Pedagogik xodimlar",
+    ru: "Педагогический состав",
+  },
+  // Footer: Подвал сайта
+  footer_title1: {
+    uz: "Universitet",
+    ru: "Университет",
+  },
+  footer_title2: {
+    uz: "Ijtimoiy tarmoq",
+    ru: "Соцсети",
+  },
+  univer_link1: {
+    uz: "Xalqaro va mahalliy munosabatlar",
+    ru: "Международные и местные связи",
+  },
+  univer_link2: {
+    uz: "Rekvizitlar",
+    ru: "Реквизиты",
+  },
+  univer_link3: {
+    uz: "Talabalarni qabul qilish",
+    ru: "Прием студентов",
+  },
 };
 
-// Responsive__lang
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Responsive__lang  // Адаптивная версия языка под устройства
 const langArr2 = {
   // Элементы навигации
   navElem1: {
@@ -156,5 +239,65 @@ const langArr2 = {
   subtitle: {
     uz: 'Kafedra: qishki sport turlari nazariyasi va uslublari',
     ru: 'Кафедра: теории и методики зимних видов спорта',
+  },
+  // Секция с количеством студентов,преподователей и т.д
+  quantity1: {
+    uz: "Talabalar soni",
+    ru: "Количество студентов",
+  },
+  quantity2: {
+    uz: "O'qituvchilar",
+    ru: "Преподаватели",
+  },
+  quantity3: {
+    uz: "Hamkorlik",
+    ru: "Сотрудничество",
+  },
+  quantity4: {
+    uz: "Kurs",
+    ru: "Курс",
+  },
+  // название секции видах спорта кафедры
+  sports_title: {
+    uz: "Kafedraning sport turlari",
+    ru: "Спортивные виды кафедры",
+  },
+  // виды спорта
+  sports_name1: {
+    uz: "Xokkey",
+    ru: "Хоккей",
+  },
+  sports_descr1: {
+    uz: "Muz ustidagi jamoaviy sport o'yini, konkida ikki jamoa o'rtasidagi qarama-qarshilik.",
+    ru: "Командная спортивная игра на льду, противостояние двух команд на коньках.",
+  },
+  sports_name2: {
+    uz: "Chim ustida xokkey",
+    ru: "Хоккей на траве",
+  },
+  sports_descr2: {
+    uz: "Jamoaviy sport turi (har bir jamoada 11 kishi) ikkita jamoa tomonidan tayoq va to'p yordamida o'ynaydi.",
+    ru: "В командном виде спорта (по 11 человек в каждой) участвуют две команды, использующие клюшки и мячи.",
+  },
+  sports_name3: {
+    uz: "Tog' chang'i",
+    ru: "Горнолыжный ",
+  },
+  sports_descr3: {
+    uz: "Sport, uning mohiyati tog'lar bo'ylab maxsus bayroqlar va darvozalar bilan belgilangan yo'l bo'ylab chang'i uchishdir.",
+    ru: "Спорт, суть которого заключается в катании на лыжах по дороге, обозначенной специальными флажками и воротами через горы.",
+  },
+  sports_name4: {
+    uz: "Figurali uchish",
+    ru: "Фигурное катание",
+  },
+  sports_descr4: {
+    uz: "Asosiy g'oya - sportchini yoki bir juft sportchini konkida muz ustida harakatlantirish va musiqaga maxsus elementlarni bajarish.",
+    ru: "Основная идея в передвижении спортсмена или пары спортсменов по льду на коньках и выполнении специальных элементов под музыку.",
+  },
+  // Секция с преподователями
+  teachers_title: {
+    uz: "Pedagogik xodimlar",
+    ru: "Педагогический состав",
   },
 };
